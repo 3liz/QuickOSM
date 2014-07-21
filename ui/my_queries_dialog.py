ba2a3ed0f277af123af4fbc18b144f45796acc56
Etienne Trimaille <etienne.trimaille@gmail.com>
2014-07-21 18:18:26 +0200

    fix duplicate ini files

--- ui/my_queries_dialog.py
+++ ui/my_queries_dialog.py
@@ -47,6 +47,7 @@ class MyQueriesWidget(QWidget, Ui_ui_my_queries):
         self.lineEdit_search.textChanged.connect(self.textChanged)
         self.radioButton_extentLayer.toggled.connect(self.extentRadio)
         self.buttonBox_layers.button(QDialogButtonBox.Reset).clicked.connect(self.fillLayerCombobox)
+        #self.treeQueries.customContextMenuRequested.connect(self.showPopupMenu)
         
         #Fill the treeview
         self.fillTree()
