8a0244bc41a14e1a53b9f5d95328310528acca0f
Etienne Trimaille <gustrimaille@yahoo.fr>
2018-10-18 19:31:14 -0400

    improve release script

--- release.sh
+++ release.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
-echo "Make a zip"
+PLUGIN_NAME="QuickOSM"
+REMOTE_NAME="origin"
 
-cp -R QuickOSM/ QuickOSM_back/
-rm QuickOSM.zip
-cd QuickOSM/
-git clean -f
-git reset --hard HEAD
-make clean_pyc
-find . -name "*.ui" -type f -delete
-find . -name "*.qrc" -type f -delete
-find . -name "*.ts" -type f -delete
-find . -depth -name 'test' -type d -exec rm -rf '{}' \;
-rm -rf .git
-rm -rf .idea
-rm -f .gitignore
-rm -f Makefile
-rm -f QuickOSM.pro
-rm -f README.md
-rm -f release.sh
-rm -f .travis.yml
-rm -f .coverage
-rm pylintrc
-rm run-env-linux.sh
+if [ $# -eq 0 ]
+then
+    echo "Use this to tag a version and push it on the remote server."
+    echo "$0 1.2.3"
+    exit 1
+fi
 
-cd ..
-zip -r QuickOSM QuickOSM/
-rm -rf QuickOSM/
-mv QuickOSM_back/ QuickOSM/
+VERSION=$1
+METADATA=$(cat metadata.txt | grep "version=" |  cut -d '=' -f2)
+
+if [ "$METADATA" != "$VERSION" ];
+then
+    echo "The metadata file has ${METADATA} while the requested tag is ${VERSION}."
+    echo "Aborting"
+    exit 1
+fi
+
+if [ -z "$(git status --porcelain)" ];
+then
+    echo "You are going to create the new tag:"
+    echo ${VERSION}
+    echo "on the remote:"
+    echo $REMOTE_NAME
+    read -p "Are you sure you want to continue?[y/n] " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Building ${PLUGIN_NAME}.zip"
+        git archive -o ${PLUGIN_NAME}.zip HEAD
+        git tag ${VERSION}
+        git push --tags ${REMOTE_NAME}
+    fi
+else
+  echo "Git working directory is not clean. Aborting."
+  exit 1
+fi
