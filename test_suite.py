4f4db1c6827f6f1201881379242c05fe2af451be
Étienne Trimaille <gustrimaille@yahoo.fr>
2019-09-13 15:40:39 +0200

    start setting up pytest
    * use either pytest or unittest on travis and gitlab-ci
    
    * update
    
    * add pytest to QGIS docker
    
    * update
    
    * avoid import before
    
    * import unittest
    
    * use same header
    
    * fix import
    
    * switch to pytest
    
    * switch back to unittest

--- test_suite.py
+++ test_suite.py
@@ -1,37 +1,42 @@
 import sys
-import unittest
 
-from osgeo import gdal
-from qgis.PyQt import Qt
-from qgis.core import Qgis
+from QuickOSM.conftest import pytest_report_header
+
+# DO NOT IMPORT BOTH unittest AND pytest
 
 
 def _run_tests(test_suite, package_name):
-    """Core function to test a test suite.
+    """Core function to test a test suite  using UnitTest
 
     :param test_suite: Unittest test suite
     """
     count = test_suite.countTestCases()
     print('########')
+    print(pytest_report_header(None))
     print('{} tests has been discovered in {}'.format(count, package_name))
-    print('QGIS : {}'.format(Qgis.QGIS_VERSION_INT))
-    print('Python GDAL : {}'.format(gdal.VersionInfo('VERSION_NUM')))
-    print('QT : {}'.format(Qt.QT_VERSION_STR))
-    # print('Python path : {}'.format(sys.path))
     print('########')
+    import unittest
     unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(test_suite)
 
 
-def test_package(package='QuickOSM'):
+def test_package(package='QuickOSM', engine='unittest'):
     """Test package.
     This function is called by travis without arguments.
 
     :param package: The package to test.
     :type package: str
+
+    :param engine: Either 'pytest' or 'unittest'.
+    :type engine: basestring
     """
-    test_loader = unittest.defaultTestLoader
-    test_suite = test_loader.discover(package)
-    _run_tests(test_suite, package)
+    if engine == 'pytest':
+        import pytest
+        pytest.main(['.'])
+    else:
+        import unittest
+        test_loader = unittest.defaultTestLoader
+        test_suite = test_loader.discover(package)
+        _run_tests(test_suite, package)
 
 
 if __name__ == '__main__':
