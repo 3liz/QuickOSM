255e6d02c51c407016e82f4dcce66f892a84e2d9
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-10-04 10:07:25 +0200

    add sys.argv

--- ui/test/test_main_window_dialog.py
+++ ui/test/test_main_window_dialog.py
@@ -1,14 +1,17 @@
-from qgis.core import Qgis
+import sys
+
 from qgis.testing import unittest, start_app
 from qgis.testing.mocked import get_iface
 from QuickOSM.ui.main_window_dialog import MainDialog
+
+if not hasattr(sys, 'argv'):
+    sys.argv = ['']
 
 start_app()
 
 
 class TestUiMainWindow(unittest.TestCase):
 
-    @unittest.skipIf(Qgis.QGIS_VERSION_INT < 30800, 'Somehow, test not working on QGIS 3.4')
     def test_show_query(self):
         """Test we can show a query by switching tab with all params."""
         # Empty query
