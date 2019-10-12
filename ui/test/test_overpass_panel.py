a26b76530460d3c943814fd7ffa3c8f1f1094bc7
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-10-13 00:33:46 +0200

    enable old tests about last nominatim places

--- ui/test/test_overpass_panel.py
+++ ui/test/test_overpass_panel.py
@@ -1,5 +1,10 @@
-import unittest
-from ..QuickOSMWidget import QuickOSMWidget
+from qgis.testing import unittest, start_app
+from qgis.testing.mocked import get_iface
+
+from ...definitions.gui import Panels
+from ..dialog import Dialog
+
+start_app()
 
 __copyright__ = 'Copyright 2019, 3Liz'
 __license__ = 'GPL version 3'
@@ -9,41 +14,37 @@ __revision__ = '$Format:%H$'
 
 class TestQuickOSMWidget(unittest.TestCase):
 
-    def setUp(self):
-        self.widget = QuickOSMWidget()
-
-    def tearDown(self):
-        self.widget = None
-
     def test_sort_nominatim_places(self):
         """Test if reorder last nominatim places works."""
+        dialog = Dialog(get_iface())
+
         existing_places = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-        new_list = self.widget.sort_nominatim_places(existing_places, '3')
+        new_list = dialog.external_panels[Panels.QuickQuery].sort_nominatim_places(existing_places, '3')
         expected = ['3', '1', '2', '4', '5', '6', '7', '8', '9', '10']
         self.assertListEqual(expected, new_list)
 
         existing_places = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-        new_list = self.widget.sort_nominatim_places(existing_places, '11')
+        new_list = dialog.external_panels[Panels.QuickQuery].sort_nominatim_places(existing_places, '11')
         expected = ['11', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.assertListEqual(expected, new_list)
 
         existing_places = ['1', '2', '3', '4', '5']
-        new_list = self.widget.sort_nominatim_places(existing_places, '3')
+        new_list = dialog.external_panels[Panels.QuickQuery].sort_nominatim_places(existing_places, '3')
         expected = ['3', '1', '2', '4', '5']
         self.assertListEqual(expected, new_list)
 
         existing_places = ['1', '2', '3', '4', '5']
-        new_list = self.widget.sort_nominatim_places(existing_places, '6')
+        new_list = dialog.external_panels[Panels.QuickQuery].sort_nominatim_places(existing_places, '6')
         expected = ['6', '1', '2', '3', '4', '5']
         self.assertListEqual(expected, new_list)
 
         existing_places = ['1', '2', '3', '4', '5']
-        new_list = self.widget.sort_nominatim_places(existing_places, '1')
+        new_list = dialog.external_panels[Panels.QuickQuery].sort_nominatim_places(existing_places, '1')
         expected = ['1', '2', '3', '4', '5']
         self.assertListEqual(expected, new_list)
 
         existing_places = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-        new_list = self.widget.sort_nominatim_places(existing_places, '1')
+        new_list = dialog.external_panels[Panels.QuickQuery].sort_nominatim_places(existing_places, '1')
         expected = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
         self.assertListEqual(expected, new_list)
 
