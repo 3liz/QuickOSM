13e318570b4d35bb731f0d5e0cf25e5315a4c06a
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-06-29 08:59:01 +0200

    Tests - Always setup the plugin when launching docker

--- .docker/start.sh
+++ .docker/start.sh
@@ -6,8 +6,6 @@ FILE="docker-compose-qgis.yml"
 docker-compose -f ${FILE} up -d --force-recreate --remove-orphans
 echo "Wait 10 seconds"
 sleep 10
-if [ "$WITH_QGIS" = with-qgis ]; then
-  echo "Installation of the plugin ${PLUGIN_NAME}"
-  docker exec -t qgis sh -c "qgis_setup.sh ${PLUGIN_NAME}"
-fi
+echo "Installation of the plugin ${PLUGIN_NAME}"
+docker exec -t qgis sh -c "qgis_setup.sh ${PLUGIN_NAME}"
 echo "Containers are running"
