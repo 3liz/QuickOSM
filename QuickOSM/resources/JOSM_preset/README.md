d9af09cc35f83fee85b7a69d9c8f97c8fe166a8d
Etienne Trimaille <gustrimaille@yahoo.fr>
2025-01-06 13:57:53 +0100

    Add scripts for JOSM presets

---
+++ QuickOSM/resources/JOSM_preset/README.md
@@ -0,0 +1,6 @@
+# Update presets
+
+Website : https://josm.openstreetmap.de/wiki/Presets
+Presets : https://github.com/simonpoole/beautified-JOSM-preset/releases
+
+Launch `./update.sh` by checking first the version inside with the latest release on GitHub
--- QuickOSM/resources/JOSM_preset/update.sh
+++ QuickOSM/resources/JOSM_preset/update.sh
@@ -1,10 +1,8 @@
-# Update presets
+#!/bin/bash
 
-Website : https://josm.openstreetmap.de/wiki/Presets
-Presets : https://github.com/simonpoole/beautified-JOSM-preset/releases
+set -e
 
-```bash
-cd QuickOSM/resources/JOSM_preset
+#cd QuickOSM/resources/JOSM_preset
 wget https://josm.openstreetmap.de/export/HEAD/josm/trunk/resources/data/defaultpresets.xml -O defaultpresets.xml
 wget https://github.com/simonpoole/beautified-JOSM-preset/releases/latest/download/josm_preset.xml -O josm_preset.xml
 wget https://github.com/simonpoole/beautified-JOSM-preset/releases/latest/download/josm_preset_orig_icons.xml -O josm_preset_orig_icons.xml
@@ -19,4 +17,3 @@ mv /tmp/vespucci/*.po ../i18n
 echo ${VERSION} > ../i18n/version.txt*
 echo ${VERSION} > version.txt
 cd .. && zip i18n.zip -r i18n
-```
