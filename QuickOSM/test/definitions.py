470909b7a675491e44f4165a3cc5521634637966
Etienne Trimaille <gustrimaille@yahoo.fr>
2025-04-22 14:28:30 +0200

    Tests - Update values in tests about "La Souterraine"

--- QuickOSM/test/definitions.py
+++ QuickOSM/test/definitions.py
@@ -3,5 +3,6 @@ __license__ = 'GPL version 3'
 __email__ = 'info@3liz.org'
 
 # For "La Souterraine" town
+# It used in tests, these values must be changed from time to time :/
 TOWN_NAME = 'La Souterraine'
-TOWN_COORDS = '46.2374832,1.4857176'
+TOWN_COORDS = '46.2308304,1.4836361'
